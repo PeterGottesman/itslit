51a10e91be6dc5b11fd8b7b07ffb86e5a8b6c7a0
Liam Gensel <liamgens@gmail.com>
2019-02-17 03:21:01 -0500

    Mr Stark, I don't feel so good... -Peter

--- src/render.c
+++ src/render.c
@@ -1,9 +1,11 @@
 #include "render.h"
 #include "defs.h"
+#include "ray.h"
 
 void render_in_inventory(SDL_Renderer *renderer, int index);
 void render_placeable(SDL_Renderer *renderer, int x, int y);
 void render_light_source(SDL_Renderer *renderer, int x, int y);
+void render_ray(SDL_Renderer *renderer, int x1, int y1, int x2, int y2);
 
 void render_level(SDL_Renderer *renderer, level_t *level)
 {
@@ -13,6 +15,11 @@ void render_level(SDL_Renderer *renderer, level_t *level)
 	x = level->light_sources[i].x;
 	y = level->light_sources[i].y;
 	render_light_source(renderer, x, y);
+    // render_ray(renderer, level->light_sources[i].x, level->light_sources[i].y, 0, 0);
+    // render_ray(renderer, level->light_sources[i].x, level->light_sources[i].y, 0, HEIGHT);
+    // render_ray(renderer, level->light_sources[i].x, level->light_sources[i].y, WIDTH, 0);
+    // render_ray(renderer, level->light_sources[i].x, level->light_sources[i].y, WIDTH, HEIGHT);
+	render_placeable(renderer, 50, 50);
     }
 
     for (int i = 0; i < level->num_placeables; ++i)
@@ -52,3 +59,11 @@ void render_placeable(SDL_Renderer *renderer, int x, int y)
     SDL_RenderFillRect(renderer, &rect);
 }
 
+void render_ray(SDL_Renderer *renderer, int x1, int y1, int x2, int y2)
+{
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
+    point_t* dest_point = cast_ray(x1, y1, x2, y2);
+    SDL_RenderDrawLine(renderer, x1, y1, dest_point->x, dest_point->y);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+
+}
